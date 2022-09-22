6cc8d30a26f52de1aac9263f550ca6b6f0fbb3fd
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-09-22 18:20:00 -0300

    removed build

--- build/Algorithm.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Algorithm = void 0;
-class Algorithm {
-    constructor(name) {
-        this.name = name;
-    }
-}
-exports.Algorithm = Algorithm;
